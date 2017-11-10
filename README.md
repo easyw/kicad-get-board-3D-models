361c11faf0afcd40da00945a7424d15abc6826a9
easyw <easyw@katamail.com>
2017-11-10 13:25:54 +0100

    initial release for win os

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # kicad-get-board-3D-models
+
+####Very initial release: 
+The script can be launched ATM only on win OS
+
+
+Installing
+----------
+- Download and unzip the files (*collect3Dmod.bat*, *list_models.py*)
+- configure the batch file with yours settings
+- run the batch on a **saved** kicad_pcb file
+
+Parameters
+----------
+- kicad_folder
+  (i.e. kicad_folder="C:\Program Files\KiCad")
+- kicad_3D_folder 
+  (i.e. kicad_3D_folder="C:\Program Files\KiCad\share\kicad\modules\packages3d")
+- kicad_version
+  (i.e. kicad_version="4" for stable, kicad_version="5" for dev)
+- kicad_board
+  (i.e. kicad_board="c:\Temp\blinky.kicad_pcb")
+- force_update
+  (i.e. force_update="/u" to force overwriting local 3D models from github lib; leave it empty to download only missing 3D models)
+
+**NB** if you configure force_update="/u" your old 3D models will get overwrite without confirmation
+ 
+**Hint:** if your kicad 3D models folder is in 'Program' folder, you need to run this batch/script as Administrator
+
+
+### License
+[GNU GENERAL PUBLIC LICENSE v3](https://www.gnu.org/licenses/gpl-3.0.html)
---
+++ collect3Dmod.bat
@@ -0,0 +1,40 @@
+@echo off
+
+echo This script will download or update 3D models of the pcb file from the github repo
+echo.
+
+cd %~dp0
+SETLOCAL
+set kicad_folder="C:\Program Files\KiCad"
+:: your KiCad installation folder
+
+set kicad_3D_folder="C:\Program Files\KiCad\share\kicad\modules\packages3d"
+:: set kicad_3D_folder="c:\kicad-stable\share\kicad\modules\packages3d"
+:: your KiCad 3D models installation folder
+
+set kicad_version="4"
+:: KiCad version (4 or 5 for dev release)
+
+set kicad_board="c:\Temp\blinky.kicad_pcb"
+:: the kicad_pcb file full path you want to fetch
+:: you can also pass this as parameter to the batch file
+if "%1"=="" (echo using internal configured 'kicad_pcb' file) else (set kicad_board=%1)
+
+::set force_update="/u"
+:: set force_upd=/u for overwrite old release of 3D models, 
+set force_update=
+:: set force_update= for download only missing models
+
+::if "%kicad_version%"=="4" (set kicad_lib=%kicad_folder%\share\kicad\modules\packages3d) else (set kicad_lib=%kicad_folder%\share\kicad\modules\packages3d)
+
+echo running on '%kicad_board%'
+echo.
+%kicad_folder%\bin\python list_models.py %kicad_3D_folder% %kicad_version% %kicad_board% %force_update%
+
+echo.
+echo HINT: if your kicad 3D models folder is in 'Program' folder, you need to run this batch as Administrator
+echo.
+
+ENDLOCAL
+
+pause
